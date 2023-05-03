de7e8606c7d46fde5f6388525bfec45a940b90eb
Karjhan <alex_mar1999@yahoo.com>
2023-05-03 09:48:47 +0300

    [ alex ] added IRepository interface

---
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace PizzaCuDeToateAPI.Repositories;
+
+public interface IRepository<T>
+{
+    public IEnumerable<T> GetAll();
+
+    public T? GetSingle(int id);
+
+    public T? AddSingle(T elemToAdd);
+
+    public void DeleteAll();
+
+    public IEnumerable<T> DeleteSingle(int id);
+
+    public T? UpdateSingle(T newElem);
+}
