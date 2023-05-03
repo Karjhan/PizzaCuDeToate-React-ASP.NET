49562620af914ffca86573eca0141cfe00bfcdd4
Karjhan <alex_mar1999@yahoo.com>
2023-05-03 09:52:20 +0300

    [ alex ] added repositories for category, stockitem and fooditem classes

---
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/FoodItemRepository.cs
@@ -0,0 +1,36 @@
+﻿using PizzaCuDeToateAPI.Models;
+
+namespace PizzaCuDeToateAPI.Repositories;
+
+public class FoodItemRepository : IRepository<FoodItem>
+{
+    public IEnumerable<FoodItem> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public FoodItem? GetSingle(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FoodItem? AddSingle(FoodItem elemToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<FoodItem> DeleteSingle(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FoodItem? UpdateSingle(FoodItem newElem)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/StockItemRepository.cs
@@ -0,0 +1,36 @@
+﻿using PizzaCuDeToateAPI.Models;
+
+namespace PizzaCuDeToateAPI.Repositories;
+
+public class StockItemRepository : IRepository<StockItem>
+{
+    public IEnumerable<StockItem> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public StockItem? GetSingle(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public StockItem? AddSingle(StockItem elemToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<StockItem> DeleteSingle(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public StockItem? UpdateSingle(StockItem newElem)
+    {
+        throw new NotImplementedException();
+    }
+}
