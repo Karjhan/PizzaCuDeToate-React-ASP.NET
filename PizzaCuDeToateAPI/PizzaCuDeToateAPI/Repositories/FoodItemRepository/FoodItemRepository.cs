c8cd76e9a608c4efec61290895731a51847b5c8b
Karjhan <alex_mar1999@yahoo.com>
2023-05-03 14:39:09 +0300

    [ alex ] changed fooditem and stockitem repo to accomodate db

--- PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/FoodItemRepository/FoodItemRepository.cs
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/FoodItemRepository/FoodItemRepository.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using PizzaCuDeToateAPI.Models;
 
 namespace PizzaCuDeToateAPI.Repositories.FoodItemRepository;
 
-public class FoodItemRepository : Repository<FoodItem>, IFoodItemRepository
+public class FoodItemRepository : IRepository<FoodItem>, IFoodItemRepository
 {
-    public FoodItemRepository(DbContext context) : base(context)
+    private readonly DbContext Context;
+
+    public FoodItemRepository( DbContext context )
     {
+        Context = context;
+    }
+
+    public IEnumerable<FoodItem> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public FoodItem? GetSingle(Expression<Func<FoodItem, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public FoodItem? AddSingle(FoodItem elemToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<FoodItem> DeleteSingle(FoodItem elemToDelete)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateSingle(FoodItem newElem)
+    {
+        throw new NotImplementedException();
     }
 }
--- PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/StockItemRepository/StockRepository.cs
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/StockItemRepository/StockRepository.cs
@@ -1,11 +1,45 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 using PizzaCuDeToateAPI.Models;
 
 namespace PizzaCuDeToateAPI.Repositories.StockItemRepository;
 
-public class StockRepository : Repository<StockItem>, IStockRepository
+public class StockRepository : IRepository<StockItem>, IStockRepository
 {
-    public StockRepository(DbContext context) : base(context)
+    private readonly DbContext Context;
+
+    public StockRepository( DbContext context )
     {
+        Context = context;
+    }
+
+    public IEnumerable<StockItem> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public StockItem? GetSingle(Expression<Func<StockItem, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public StockItem? AddSingle(StockItem elemToAdd)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<StockItem> DeleteSingle(StockItem elemToDelete)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateSingle(StockItem newElem)
+    {
+        throw new NotImplementedException();
     }
 }
