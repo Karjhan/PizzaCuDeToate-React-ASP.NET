aed547c752083b02a4ecbcf7a34684a6472032db
Ioana <dumitrescuioana20@gmail.com>
2023-05-04 21:50:56 +0300

    Added JSONStockItemDTO.cs and UpdateStockItemDTO.cs;

---
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/DTOClasses/UpdateStockItemDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace PizzaCuDeToateAPI.DTOClasses;
+
+public class UpdateStockItemDTO
+{
+    public string Name { get; set; } 
+    
+    public bool IsIngredient { get; set; }
+    
+    public string QuantityPerUnit { get; set; } 
+    
+    public double UnitPrice { get; set; }
+    
+    public int UnitsInStock { get; set; }
+
+    public string Logo { get; set; } 
+}
