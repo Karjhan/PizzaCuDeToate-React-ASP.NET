18c27bab00d217cccee4e684273ff8ec622825d6
Ioana <dumitrescuioana20@gmail.com>
2023-05-04 10:53:23 +0300

    Added StockItemDTO.cs;

---
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/DTOClasses/StockItemDTO.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Build.Framework;
+using PizzaCuDeToateAPI.Models;
+
+namespace PizzaCuDeToateAPI.DTOClasses;
+
+public class StockItemDTO
+{
+    [Required]
+
+    public string Name { get; set; } = string.Empty;
+    
+    public bool IsIngredient { get; set; }
+    
+    public string QuantityPerUnit { get; set; } = string.Empty;
+    
+    public double UnitPrice { get; set; }
+    
+    public int UnitsInStock { get; set; }
+
+    public string Logo { get; set; } 
+}
