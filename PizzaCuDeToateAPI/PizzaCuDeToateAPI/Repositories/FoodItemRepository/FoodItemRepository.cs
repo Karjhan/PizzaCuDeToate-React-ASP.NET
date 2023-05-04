da7de90bcc3515a3b5c464266be5655fb83057ac
Karjhan <alex_mar1999@yahoo.com>
2023-05-04 13:34:46 +0300

    [ alex ] removed include Logo from foodItem repo get and getall

--- PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/FoodItemRepository/FoodItemRepository.cs
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Repositories/FoodItemRepository/FoodItemRepository.cs
@@ -17,12 +17,12 @@ public class FoodItemRepository : IRepository<FoodItem>, IFoodItemRepository
 
     public IEnumerable<FoodItem> GetAll()
     {
-        return Context.FoodItems.Include(foodItem => foodItem.Ingredients).Include(foodItem => foodItem.Logo).ToList();
+        return Context.FoodItems.Include(foodItem => foodItem.Ingredients).ToList();
     }
 
     public FoodItem? GetSingle(Expression<Func<FoodItem, bool>> predicate)
     {
-        return Context.FoodItems.Where(predicate).Include(foodItem => foodItem.Ingredients).Include(foodItem => foodItem.Logo).FirstOrDefault();
+        return Context.FoodItems.Where(predicate).Include(foodItem => foodItem.Ingredients).FirstOrDefault();
     }
 
     public FoodItem? AddSingle(FoodItem elemToAdd)
