582c8370ae1c1e2343177de5955fc450ffb5d46a
HoriaMitrica <horia.mitrica@gmail.com>
2023-05-10 20:07:23 +0300

    [ horia ] backend-frontend connection done

--- PizzaCuDeToateAPI/PizzaCuDeToateAPI/Program.cs
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Program.cs
@@ -20,6 +20,17 @@ builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddTransient<DbContext, ApplicationContext>();
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PizzaCuDeToate_Db")));
 
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddCors(options =>
+{
+    var frontendURL = configuration.GetValue<string>("frontend_url");
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(frontendURL).AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,6 +41,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
