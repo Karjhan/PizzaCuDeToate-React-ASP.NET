85cdab106e93ea867a57a0f609b6ff0c009bd2a1
Ioana <dumitrescuioana20@gmail.com>
2023-05-15 09:25:38 +0300

    Added Order.cs and OrderDetails.cs classes;

---
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace PizzaCuDeToateAPI.Models;
+
+public class Order
+{
+    public int OrderId { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Address { get; set; }
+    
+    public DateTime OrderPlacedTime { get; set; }
+
+    public int UserId { get; set; }
+}
---
+++ PizzaCuDeToateAPI/PizzaCuDeToateAPI/Models/OrderDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace PizzaCuDeToateAPI.Models;
+
+public class OrderDetails
+{
+    public int Id { get; set; }
+
+    public Order OrderId { get; set; }
+
+    public FoodItem FoodItemId { get; set; }
+
+    public StockItem StockItemId { get; set; }
+
+    public int TotalQuantityProducts { get; set; }
+}
